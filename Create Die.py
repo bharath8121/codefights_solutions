34ee90c9a5afdad35c17a105cb3ecf9c9248f7b4
Bharath Chandra <bharathchandra.chandra96@gmail.com>
2018-09-17 20:08:30 -0700

    Create Create Die.py

---
+++ Create Die.py
@@ -0,0 +1,12 @@
+import random
+
+def createDie(seed, n):
+    class Die(object):
+        def __new__(cls, s,n):
+            random.seed(s)
+            return int(random.random() * n) + 1
+
+    class Game(object):
+        die = Die(seed, n)
+
+    return Game.die
